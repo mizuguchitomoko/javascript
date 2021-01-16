a778326f957fe48ee2815a9bb43a01e14f3242b6
水口知子 <mizuguchitomoko@MacBook-Pro.local>
2021-01-17 07:57:47 +0900

    slideshow

--- hamburger.js
+++ hamburger.js
@@ -7,3 +7,16 @@ function hamburger() {
 document.getElementById("hamburger").addEventListener("click", function () {
   hamburger();
 });
+const pics_src = ["cafe.jpg", "coffee.jpg", "beans.jpg"];
+let num = -1;
+
+function slideshow_timer() {
+  if (num === 2) {
+    num = 0;
+  } else {
+    num++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+
+setInterval(slideshow_timer, 1500);
